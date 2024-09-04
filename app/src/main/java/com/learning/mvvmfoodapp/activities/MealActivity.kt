e89933c58566d2ad66ef61795bcd1983d4f4d5ba
MalakAlKazem <123973743+MalakAlKazem@users.noreply.github.com>
2024-09-04 22:17:36 +0300

    Added Room

--- app/src/main/java/com/learning/mvvmfoodapp/activities/MealActivity.kt
+++ app/src/main/java/com/learning/mvvmfoodapp/activities/MealActivity.kt
@@ -57,7 +57,7 @@ class MealActivity : AppCompatActivity() {
                 binding.tvCategory.text = "Category: ${meal!!.strCategory}"
                 binding.tvArea.text = "Area: ${meal!!.strArea}"
                 binding.tvInstructionsSteps.text = meal.strInstructions
-                youtubeLink = meal.strYoutube
+                youtubeLink = meal.strYoutube!!
             }
         })
     }
